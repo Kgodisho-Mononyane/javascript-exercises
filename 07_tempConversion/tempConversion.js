3d4b64a91ad915540be98675d1da9dcc25d2dd78
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2024-12-24 13:04:07 +0200

    complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,15 +1,9 @@
 const convertToCelsius = function(fahrenheit) {
-  let celsius = ((fahrenheit * 9/5) + 32);
-  return celsius;
-  //return celsius.round(num * 10)/10;
+  return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10
 };
-console.log(convertToCelsius(32));
-
-//Math.round(number * 10) / 10
 
 const convertToFahrenheit = function(celsius) {
-  // let fahrenheit = ((num - 32)* 5/9);
-  // return fahrenheit.round(num * 10)/10;
+  return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
