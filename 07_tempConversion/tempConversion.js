1d0f2cfc8a782f35db2252d30d479e157a0bd3e4
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2024-12-24 10:01:49 +0200

    added to convertToCelsius and convertToFahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = ((fahrenheit * 9/5) + 32);
+  return celsius;
+  //return celsius.round(num * 10)/10;
 };
+console.log(convertToCelsius(32));
 
-const convertToFahrenheit = function() {
+//Math.round(number * 10) / 10
+
+const convertToFahrenheit = function(celsius) {
+  // let fahrenheit = ((num - 32)* 5/9);
+  // return fahrenheit.round(num * 10)/10;
 };
 
 // Do not edit below this line
