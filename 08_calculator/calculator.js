b31d9cc4f5d469d5020c98b3fca2c9a62a0d305e
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2025-02-02 12:10:34 +0200

    complete calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,19 +6,24 @@ const subtract = function(a, b) {
   return a - b
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function(a, b) {
-  return a * b
+const multiply = function (array) {
+  return array.reduce((product, current) => product * current)
 };
 
 const power = function(a, b) {
-	a ** b
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  if (n == 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 };
 
 // Do not edit below this line
