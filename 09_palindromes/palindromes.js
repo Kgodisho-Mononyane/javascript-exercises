d968f56bf1000f9becbe179d4b1926fe8dddee3e
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2025-02-02 12:56:50 +0200

    add to palindrome.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let string = str
+    .toLowerCase()
+    .split('') //set a value of the string of just letters
+    let reverseString = string//set the value of the reverse string of just letters
 
-};
+    return string === reverseString;
+}
+
 
 // Do not edit below this line
 module.exports = palindromes;
