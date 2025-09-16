0f88fc900d1b7730298ef42aaf58d8c60e3627fc
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2025-09-16 18:36:32 +0200

    complete removeFromArray.js

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const newArray = [];
+
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    })
+    return newArray;
+    //write a function that takes an array and removes any instance of the number within that array and returns the
+    //new array without those instances
 };
 
 // Do not edit below this line
