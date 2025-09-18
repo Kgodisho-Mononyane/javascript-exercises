6fa1a3ebea20344c316f5dd4b87754dcd648f6a7
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2025-09-18 09:23:52 +0200

    complete leapYears.spec.js

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
+        return true;
+    } else if (year % 4 === 0 && year % 100 === 0) {
+        return false;
+    } else if (year % 4 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+    
+    //divisible by 4 = leap year
+    //divisible by 4 && divisible by 100 = !leap year
+    //divisible by 4 && divisible by 100 && divisible by 400 == leap year
 
 };
 
