b120bffe3dea48303ddac414ad6ac89e68ac0c75
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2026-03-31 10:39:56 +0200

    add comment for contains.js

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,11 @@
-const contains = function() {
-  
+const contains = function(obj, val) {
+    /*
+    if (obj.includes(val)) {
+        return true;
+    } else {
+        return (ojb.)
+    }*/
+   //Check for recursion that summed salaries
 };
   
 // Do not edit below this line
