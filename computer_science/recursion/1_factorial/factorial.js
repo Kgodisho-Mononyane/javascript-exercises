ee022c212f82df6e490f20cb669039a618d0175b
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2026-03-31 10:31:57 +0200

    complete factorial.js

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,6 +1,15 @@
-const factorial = function() {
-
+const factorial = function(n) {
+    
+    if (typeof(n) !== "number" || n < 0 || n % 1 !== 0) {
+        return undefined;
+    } else if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
 };
+
+console.log(factorial(0))
 
 // Do not edit below this line
 module.exports = factorial;
